08d657bb810bf8652efe2fedbd094463e372ce39
ammar naser <ammarabdelnasertag@gmail.com>
2022-07-04 21:34:06 +0200

    first problem solution

---
+++ DS-study-plan/Contains-Duplicate.js
@@ -0,0 +1,27 @@
+var containsDuplicate = function (nums) {
+  const num = nums.sort(function (a, b) {
+    return a - b;
+  });
+
+  for (let i = 0; i < num.length; i++) {
+    if (num[i] === num[i + 1]) {
+      return true;
+    }
+  }
+  return false;
+};
+
+// solution with hashMap.
+
+var containsDuplicate = function (nums) {
+  const num = new Map();
+  for (let i = 0; i < nums.length; i++) {
+    if (num.has(nums[i])) {
+      return true;
+    }
+
+    num.set(nums[i]);
+  }
+
+  return false;
+};
